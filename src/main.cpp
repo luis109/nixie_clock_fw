47a4ec6f77a752bac6eeb018b3c1fd848f3ce3a5
Luis Venancio <venancio@lsts.pt>
2024-11-15 17:01:07 +0000

    Time acquisition prevented device from restart.

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ void setup()
 
   // Init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  while (getLocalTimeString() == "")
-    delay(5000);
+  // while (getLocalTimeString() == "")
+  //   delay(5000);
 
   // Init web server
   g_ws_manager.updateTimeString(getLocalTimeString());
