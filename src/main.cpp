809d723a09519302526a99ab6e3e12915f26a48f
Luís Venâncio <venancio@lsts.pt>
2024-11-16 01:27:08 +0000

    Moved internet time management to ServerManager class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,27 @@
 #include <Arduino.h>
 #include "ServerManager.hpp"
-#include "time.h"
 
 ServerManager g_ws_manager;
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
-const int   daylightOffset_sec = 3600;
 
 unsigned long lastTime = 0;  
 unsigned long timerDelay = 500;  // send readings timer
 
-String 
-getLocalTimeString(){
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return String();
-  }
+// String 
+// getLocalTimeString(){
+//   struct tm timeinfo;
+//   if (!g_ws_manager.getInternetTime(&timeinfo))
+//     return String();
   
-  char time_str[64];
-  strftime(time_str, 64, "%A, %B %d %Y %H:%M:%S", &timeinfo);
-
-  return String(time_str);
-}
+//   char time_str[64];
+//   strftime(time_str, 64, "%A, %B %d %Y %H:%M:%S", &timeinfo);
+//   return String(time_str);
+// }
 
 void setup() 
 {
-  g_ws_manager.begin();
-
-  // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  // while (getLocalTimeString() == "")
-  //   delay(5000);
-
   // Init web server
-  g_ws_manager.updateTimeString(getLocalTimeString());
+  g_ws_manager.begin();
+  g_ws_manager.updateTimeString(g_ws_manager.getInternetTimeStr());
 }
 
 void loop() 
@@ -48,7 +34,7 @@ void loop()
 
   if ((millis() - lastTime) > timerDelay) 
   {
-    g_ws_manager.updateTimeString(getLocalTimeString());
+    g_ws_manager.updateTimeString(g_ws_manager.getInternetTimeStr());
     g_ws_manager.run();
     
     lastTime = millis();
