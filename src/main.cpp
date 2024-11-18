7f4c334b44e20d69035a81dbb9363fcb62babada
Luís Venâncio <venancio@lsts.pt>
2024-11-18 00:36:06 +0000

    Cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,11 @@
 ServerManager g_ws_manager;
 DisplayDriver g_ddriver;
 
-unsigned long lastTime = 0;  
-unsigned long timerDelay = 500;  // send readings timer
-
-// String 
-// getLocalTimeString(){
-//   struct tm timeinfo;
-//   if (!g_ws_manager.getInternetTime(&timeinfo))
-//     return String();
-  
-//   char time_str[64];
-//   strftime(time_str, 64, "%A, %B %d %Y %H:%M:%S", &timeinfo);
-//   return String(time_str);
-// }
-
 void setup() 
 {
   // Init web server
   g_ws_manager.begin();
-  g_ws_manager.updateTimeString(g_ws_manager.getInternetTimeStr());
+
 
   g_ddriver.begin();
   g_ddriver.setDisplay(12, 34, 56);
@@ -37,13 +23,6 @@ void loop()
     ESP.restart();
   }
 
-  if ((millis() - lastTime) > timerDelay) 
-  {
-    g_ws_manager.updateTimeString(g_ws_manager.getInternetTimeStr());
-    g_ws_manager.run();
-    
-    lastTime = millis();
-  }
-
+  g_ws_manager.run();
   g_ddriver.run();
 }
